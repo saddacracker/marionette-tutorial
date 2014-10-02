8015b20717480fa25eb4b30f1598f49208c14cb3
John Cardwell <cardwell.john@gmail.com>
2014-10-02 15:51:57 -0700

    Creating a Module for ContactsApp (b)

---
+++ public/javascripts/apps/contacts/list/list_controller.js
@@ -0,0 +1,19 @@
+ContactManager.module("ContactsApp.List", function(List, ContactManager, 
+Backbone, Marionette, $, _){
+    
+    
+    List.Controller = {
+        listContacts: function() {
+            var contacts = ContactManager.request("contact:entities");
+    
+            // Marrionette collection view
+            var contactsListView = new List.Contacts({
+                collection: contacts
+            });
+    
+            ContactManager.mainRegion.show(contactsListView);
+        }
+    }
+    
+    
+});
